14844c1bba8e65919bbefb9f82e9c6c6ff899c46
Sebastian Kühnau <seb@vaadin.com>
2023-04-12 16:16:49 +0200

    updating the theme

--- src/main/java/com/example/application/views/main/AdminView.java
+++ src/main/java/com/example/application/views/main/AdminView.java
@@ -4,7 +4,9 @@ import com.example.application.data.service.TopicService;
 import com.example.application.data.service.VaadinerService;
 import com.example.application.views.main.components.TopicAdminLayout;
 import com.example.application.views.main.components.TopicLayout;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -23,7 +25,11 @@ public class AdminView extends Div {
         this.topicService = topicService;
         setClassName("admin");
         topicLayout = new TopicAdminLayout(topicService, vaadinerService);
-        add(new Span("Topic administration"));
+        H1 topicAdministration = new H1("Topic administration");
+        topicAdministration.getStyle().set("text-align", "center");
+        add(topicAdministration);
+        addClassName("event-header");
         add(topicLayout);
+        setSizeFull();
     }
 }
